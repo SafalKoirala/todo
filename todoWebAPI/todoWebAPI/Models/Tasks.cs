24f85d71a41650739b8396cd8e04414dff989ca2
SafalKoirala <safalkoirala92@gmail.com>
2021-07-06 16:16:32 +0545

    initial models created

---
+++ todoWebAPI/todoWebAPI/Models/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace todoWebAPI.Models
+{
+    public class Tasks
+    {
+        public int TaskID { get; set; }
+        public int TaskNameID { get; set; }
+        public string Alltasks { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
