c9397196aabb2b1b54dc3dcf045b958116b568c4
SafalKoirala <safalkoirala92@gmail.com>
2021-07-06 14:50:21 +0545

    initial commit

---
+++ todoWebAPI/todoWebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+namespace todoWebAPI
+{
+    public static class WebApiConfig
+    {
+        //database connectivity
+        public static MySqlConnection conn()
+        {
+            string sqlConfig = "server=localhost; port=3306; database=todo; username=root;";
+            MySqlConnection conn = new MySqlConnection(sqlConfig);
+            return conn;
+        }
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
