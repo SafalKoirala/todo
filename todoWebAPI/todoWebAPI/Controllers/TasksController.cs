388034d10f6781009cdf4f7525e44762c8d6fff2
SafalKoirala <safalkoirala92@gmail.com>
2021-07-07 21:58:50 +0545

    taskModel created

---
+++ todoWebAPI/todoWebAPI/Controllers/TasksController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using todoWebAPI.Models;
+
+namespace todoWebAPI.Controllers
+{
+    public class TasksController : ApiController
+    {
+        Tasks task = new Tasks();
+        public HttpResponseMessage Get(int id)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = task.listTasks(id);
+                return Request.CreateResponse(HttpStatusCode.OK, dt);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.OK, e);
+            }
+        }
+
+       /* public HttpResponseMessage Get(int id)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = task.taskInformation(id);
+                return Request.CreateResponse(HttpStatusCode.OK, dt);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.OK, e);
+            }
+        }
+
+        public string Post(Tasks taskData)
+        {
+            try
+            {
+                task.addTasks();
+                return "Data Added successfully !!";
+            }
+            catch (Exception e)
+            {
+                return "Failed to insert data: Exception :" + e;
+            }
+        }
+
+        public string Put(Tasks taskData)
+        {
+            try
+            {
+                task.updateTask(taskData);
+                return "Updated Data successfull";
+            }
+            catch (Exception e)
+            {
+                return "Failed to Update data: Exception :" + e;
+            }
+        }
+
+        public string Delete(Tasks taskData)
+        {
+            try
+            {
+                task.deleteTask(taskData);
+                return "Task Removed Successfully";
+            }
+            catch (Exception e)
+            {
+                return "Delete Failed";
+            }
+        }*/
+    }
+}
--- todoWebAPI/todoWebAPI/Models/Tasks.cs
+++ todoWebAPI/todoWebAPI/Models/Tasks.cs
@@ -1,45 +1,29 @@
 ﻿using System;
 using System.Data;
 using MySql.Data.MySqlClient;
-
 namespace todoWebAPI.Models
 {
-    public class Users : BaseModel
+    public class Tasks :BaseModel
     {
+        public int taskId { get; set; }
         public int userId { get; set; }
-        public string userName { get; set; }
-        public string password { get; set; }
+        public string taskName { get; set; }
+        public bool isCompleted { get; set; }
 
-        public Users() { }
-        public Users(string userName, string password)
+        public Tasks () { }
+
+        public DataTable listTasks( int userId)
         {
-            this.userName = userName;
-            this.password = password;
-        }
-
-        public Users(int uid, string userName)
-        {
-            this.userId = uid;
-            this.userName = userName;
-        }
-
-        public Users(int userId)
-        {
-            this.userId = userId;
-        }
-
-
-        public DataTable listUsers()
-        {
-            MySqlCommand cmd = new MySqlCommand("SELECT userId, userName FROM users", con);
+            MySqlCommand cmd = new MySqlCommand("SELECT taskId, taskName, isCompleted FROM tasks WHERE userId=@userId", con);
             cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@userId", MySqlDbType.Int32, sizeof(Int32)).Value = userId;
             MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             sda.Fill(dt);
             return dt;
         }
 
-        public void addUser(Users user)
+        /*public void addUser(Users user)
         {
             string query = @"Insert into users (userName,password) values(@userName, @password)";
             MySqlCommand cmd = new MySqlCommand(query, con);
@@ -74,11 +58,11 @@ namespace todoWebAPI.Models
             sda.Fill(dt);
         }
 
-        public DataTable userInformation(int userId)
+        public DataTable userInformation(int uid)
         {
             string query = @"SELECT userId, userName FROM Users where userId = @userId";
             MySqlCommand cmd = new MySqlCommand(query, con);
-            cmd.Parameters.Add("@userId", MySqlDbType.Int32, sizeof(Int32)).Value = userId;
+            cmd.Parameters.Add("@userId", MySqlDbType.Int32, sizeof(Int32)).Value = uid;
             cmd.CommandType = CommandType.Text;
             MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -86,5 +70,6 @@ namespace todoWebAPI.Models
             return dt;
 
         }
+*/
     }
 }
